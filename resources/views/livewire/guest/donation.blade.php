c859a598583075a5d9c2694930b72a9759bbf8db
username_anjastabuni <reblytabuni2004@gmail.com>
2024-05-08 11:44:30 +0700

    Feat: Tambahkan Isi konten dalam file donasi

--- resources/views/livewire/guest/donation.blade.php
+++ resources/views/livewire/guest/donation.blade.php
@@ -23,9 +23,29 @@
           <!-- Content -->
           <div class="space-y-5 md:space-y-8">
 
-            <p class="text-lg text-gray-800 dark:text-neutral-200">This page is under development...</p>
+            <figure>
+              <img class="w-full object-cover rounded-xl" src="{{ asset('images/about-us2.jpg') }}"
+                alt="Image Description">
+              <figcaption class="mt-3 text-xs text-center text-gray-500 dark:text-neutral-500 italic">
+                Coding course documentation: Learn HTML and CSS at Nokensoft Office, in Jayapura, Papua.
+              </figcaption>
+            </figure>
+
+            <h3 class="text-2xl font-semibold dark:text-white" id="itenthusiats" >Pembelian Brand SaCode (T-Shirt, Hoodie, Sweater)</h3>
+
+            <p class="text-lg text-gray-800 dark:text-neutral-200">Dapatkan pakaian berdesain unik dari koleksi eksklusif kami! Setiap pembelian T-shirt, hoodie, 
+              atau pakaian lainnya dengan brand SaCode tidak hanya membuat Anda tampil bergaya, tetapi juga mendukung berbagai program pengembangan koding kami. 
+              Bergabunglah dalam misi kami untuk memperluas aksesibilitas pendidikan teknologi bagi semua orang.</p>
+            
+            <h3 class="text-2xl font-semibold dark:text-white" id="itenthusiats" >Sumbangan Laptop Bekas Untuk Kelas Coding</h3>
+
+            <p class="text-lg text-gray-800 dark:text-neutral-200">Laptop bekas Anda dapat menjadi sumber daya berharga untuk menciptakan peluang baru bagi mereka yang bersemangat untuk belajar koding.
+               Melalui program kursus coding kami, kami memberikan akses kepada individu yang kurang beruntung untuk memperoleh pengetahuan dan keterampilan yang diperlukan untuk mengikuti jejak karier di bidang teknologi. 
+              Sumbangan laptop bekas Anda akan menjadi langkah awal yang besar dalam perjalanan mereka.</p>
   
-        
+            <p class="text-lg font-semibold text-gray-800 dark:text-neutral-200">“Mari bersama-sama kita membangun masa depan yang lebih cerah dan inklusif melalui dunia koding. 
+              Setiap kontribusi Anda memiliki dampak besar dalam membantu kami mencapai visi kami untuk menyebarkan manfaat teknologi kepada lebih banyak orang.”</p>
+  
           </div>
           <!-- End Content -->
           
