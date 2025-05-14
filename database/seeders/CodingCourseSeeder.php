e9f125c7ac7669aef100892d03d3a3de030012a9
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2025-05-14 15:47:23 +0900

    edit seeders dan add photo in courses

--- database/seeders/CodingCourseSeeder.php
+++ database/seeders/CodingCourseSeeder.php
@@ -13,6 +13,18 @@ class CodingCourseSeeder extends Seeder
     {
         collect([
             [
+                'id' => 10,
+                'title' => 'Belajar Coding Dasar: HTML & CSS',
+                'slug' => 'belajar-coding-dasar-html-css',
+                'description' => 'Bagi kam yang mau belajar dasar-dasar coding mulai dari awal, kelas ini cocok untuk pemula atau yang mau mendalami teknik penulisan coding HTML & CSS.',
+                'thumbnail' => '2025/sacode-course-poster-05.png',
+                'wa_link' => 'https://wa.link/3esstr',
+                'is_on_site' => 'Online',
+                'is_open' => true,
+                'user_id' => fake()->numberBetween(1, 5),
+                'published_at' => now(),
+            ],
+            [
                 'id' => 9,
                 'title' => 'Belajar Coding Dasar: JavaScript',
                 'slug' => 'belajar-coding-dasar-javascript',
@@ -20,7 +32,7 @@ class CodingCourseSeeder extends Seeder
                 'thumbnail' => '2025/sacode-course-poster-04.png',
                 'wa_link' => 'https://wa.link/3esstr',
                 'is_on_site' => 'On Site',
-                'is_open' => true,
+                'is_open' => false,
                 'user_id' => fake()->numberBetween(1, 5),
                 'published_at' => now(),
             ],
@@ -32,7 +44,7 @@ class CodingCourseSeeder extends Seeder
                 'thumbnail' => '2025/sacode-course-poster-03.png',
                 'wa_link' => 'https://wa.link/3esstr',
                 'is_on_site' => 'Online',
-                'is_open' => true,
+                'is_open' => false,
                 'user_id' => fake()->numberBetween(1, 5),
                 'published_at' => now(),
             ],
